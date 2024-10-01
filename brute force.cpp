99a4ec70873b7ec789e3d2c75f1de1e0895700cd
AhmedPlusPlus <157212041+AhmedMostafa81@users.noreply.github.com>
2024-10-02 00:11:13 +0300

    Create brute force.cpp

---
+++ brute force.cpp
@@ -0,0 +1,14 @@
+    int n = 30;
+    for (int i1=1;i1<=n;i1++) {
+        for (int i2=i1+1;i2<=n;i2++) {
+            for (int i3=i2+1;i3<=n;i3++) {
+                for (int i4=i3+1;i4<=n;i4++) {
+                    for (int i5=i4+1;i5<=n;i5++) {
+                        for (int i6=i5+1;i6<=n;i6++) {
+
+                        }
+                    }
+                }
+            }
+        }
+    }
