09f76924f020f04a2dae0b1af3efc44eca5c302f
AhmedPlusPlus <157212041+AhmedMostafa81@users.noreply.github.com>
2025-03-28 06:23:02 +0200

    Update Trie.cpp

--- Trie.cpp
+++ Trie.cpp
@@ -58,3 +58,52 @@ struct Trie {
         insert(0);
     }
 };
+
+struct Trie {
+    struct Node {
+        static const int MX = 29;
+        int children[MX] = {};
+        int f = 0;
+    };
+
+    vector<Node> trie;
+
+    Trie() {
+        trie.emplace_back();
+    }
+
+    void insert(string& s) {
+        int idx = 0;
+        for(auto i : s) {
+            int nxt = i-'a';
+
+            if(trie[idx].children[nxt] == 0) {
+                trie[idx].children[nxt] = trie.size();
+                trie.emplace_back();
+            }
+            idx = trie[idx].children[nxt];
+            trie[idx].f++;
+        }
+    }
+
+    void erase(string& s) {
+        int idx = 0;
+        for(auto i : s) {
+            int nxt = i-'a';
+
+            idx = trie[idx].children[nxt];
+            trie[idx].f--;
+        }
+    }
+
+    int query(string& s) {
+        int idx = 0;
+        for(auto i : s) {
+            int nxt = i-'a';
+            if(trie[trie[idx].children[nxt]].f == 0)
+                return 0;
+            idx = trie[idx].children[nxt];
+        }
+        return trie[idx].f;
+    }
+};
