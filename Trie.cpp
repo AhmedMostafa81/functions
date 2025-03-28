43ea13890e6dfc0c810e01a75c0bbdd2cd66f30f
AhmedPlusPlus <157212041+AhmedMostafa81@users.noreply.github.com>
2025-03-28 05:21:14 +0200

    Update Trie.cpp

--- Trie.cpp
+++ Trie.cpp
@@ -1,54 +1,60 @@
-const int BIT = 30  ;
-struct Node {
-    int ch[2] = {};
-    int f = 0;
-}neutral;
- 
-vector<Node> trie;
 struct Trie {
- 
+    const int BIT = 30;
+    struct Node {
+        static const int MX = 2;
+        int children[MX] = {};
+        int f = 0;
+    };
+
+    vector<Node> trie;
+
     Trie() {
-        trie.pb(neutral);
+        trie.emplace_back();
         insert(0);
     }
- 
-    void insert (int x) {
-        int cur = 0;
-        for (int b = BIT;b>=0;b--) {
-            int nxt = ((x&(1ll<<b)) != 0);
-            if (!trie[cur].ch[nxt]) {
-                trie[cur].ch[nxt] = trie.size();
-                trie.pb(neutral);
+
+    void insert(int x) {
+        int idx = 0;
+        for(int bit = BIT; bit >= 0; bit--) {
+            int nxt = ((x&(1<<bit)) != 0);
+
+            if(trie[idx].children[nxt] == 0) {
+                trie[idx].children[nxt] = trie.size();
+                trie.emplace_back();
             }
-            cur = trie[cur].ch[nxt];
-            trie[cur].f++;
+            idx = trie[idx].children[nxt];
+            trie[idx].f++;
         }
     }
- 
+
     void erase(int x) {
         int idx = 0;
         for(int bit = BIT; bit >= 0; bit--) {
             int nxt = ((x&(1<<bit)) != 0);
- 
-            idx = trie[idx].ch[nxt];
+
+            idx = trie[idx].children[nxt];
             trie[idx].f--;
         }
     }
- 
-    int ans(int x) {
+
+    int query(int x) {
         int idx = 0;
         int ret = 0;
         for(int bit = BIT; bit >= 0; bit--) {
             int nxt = ((x&(1<<bit)) == 0);
- 
-            if(trie[trie[idx].ch[nxt]].f == 0)
+
+            if(trie[trie[idx].children[nxt]].f == 0)
                 nxt ^= 1;
             else
                 ret |= (1<<bit);
-            idx = trie[idx].ch[nxt];
+            idx = trie[idx].children[nxt];
         }
         return ret;
     }
- 
- 
+
+    void clear() {
+        trie.clear();
+        trie.emplace_back();
+        insert(0);
+    }
 };
