fa3964501a0630149bc45fc025757e8958c4decb
AhmedPlusPlus <157212041+AhmedMostafa81@users.noreply.github.com>
2025-05-10 18:29:26 +0300

    Update mo's algorithm.cpp

--- mo's algorithm.cpp
+++ mo's algorithm.cpp
@@ -1,38 +1,36 @@
-set<tuple<int,int,int,int>>mo;
-const int N = 2e5+5;
-int a[N],rs=0,L,R;
+vector<tuple<int,int,int,int>>mo;
+int a[N],L,R;
 
 void add(int idx) {
-    
+
 }
 void rem(int idx) {
-    
+
 }
 
 void pewpew() {
-
-    mo.clear();
-    rs=0,L = 1,R=0;
+    
+    L = 0,R=-1;
     int n,q;cin>>n>>q;
-    int sq = sqrt(n);
+    int sq = sqrt(n)+2;
     for (int i =0;i<n;i++)cin>>a[i];
     for (int i =0;i<q;i++) {
-        int l,r;cin>>l>>r;
+        int l,r,x;cin>>l>>r>>x;
         l--,r--;
-        mo.insert({l/sq,r,l,i});
+        mo.pb({l/sq,r,l,i});
     }
     int ans[q];
-    for (auto it = mo.begin() ; it!=mo.end() ; it++) {
-        auto [tt,r,l,idx] = *it;
+    sort(all(mo));
+    for (auto &[tt,r,l,idx]:mo) {
 
-        while(L < l)rem(L++);
         while(L > l)add(--L);
         while(R < r)add(++R);
+        while(L < l)rem(L++);
         while(R > r)rem(R--);
 
-        ans[idx] = rs; 
+        ans[idx] = ;
     }
     for (int i=0;i<q;i++)
-        cout <<ans[i];
+        cout <<ans[i] << '\n';
 
 }
