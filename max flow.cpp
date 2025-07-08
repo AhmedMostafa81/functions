c6b34dda17f6b3768ff73e5d503cf15c741d2867
AhmedPlusPlus <157212041+AhmedMostafa81@users.noreply.github.com>
2025-07-08 14:50:03 +0300

    Create max flow.cpp

---
+++ max flow.cpp
@@ -0,0 +1,77 @@
+// slow max flow
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define AhmedPlusPlus ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define hi cerr<<"HI\n";
+
+/*                        ->    NO CLEAN CODE HERE    <-                        */
+
+
+int n;
+vector<vector<int>> capacity(N , vector<int>(N));
+vector<vector<int>> adj(N);
+
+int bfs(int s, int t, vector<int>& parent) {
+    fill(parent.begin(), parent.end(), -1);
+    parent[s] = -2;
+    queue<pair<int, int>> q;
+    q.push({s, 1e9});
+
+    while (!q.empty()) {
+        int cur = q.front().first;
+        int flow = q.front().second;
+        q.pop();
+
+        for (int next : adj[cur]) {
+            if (parent[next] == -1 && capacity[cur][next]) {
+                parent[next] = cur;
+                int new_flow = min(flow, capacity[cur][next]);
+                if (next == t)
+                    return new_flow;
+                q.push({next, new_flow});
+            }
+        }
+    }
+
+    return 0;
+}
+
+int maxflow(int s, int t) {
+    int flow = 0;
+    vector<int> parent(n);
+    int new_flow;
+
+    while (new_flow = bfs(s, t, parent)) {
+        flow += new_flow;
+        int cur = t;
+        while (cur != s) {
+            int prev = parent[cur];
+            capacity[prev][cur] -= new_flow;
+            capacity[cur][prev] += new_flow;
+            cur = prev;
+        }
+    }
+
+    return flow;
+}
+
+signed main() {
+/* ^^^ */    AhmedPlusPlus    /* ^^^ */
+
+//    ->> practice makes perfect
+
+    n = ;
+
+    int q;cin>>q;
+    while(q--){
+        int x,y,c;cin>>x>>y>>c;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+        capacity[x][y] = c;
+    }
+    cout << maxflow(S , T);
+
+}
+/////////////////////////////////////////////////////////////////////////////////////////
