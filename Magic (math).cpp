0275c34d55bb2685ed45ca47569a569544c72ed0
AhmedPlusPlus <157212041+AhmedMostafa81@users.noreply.github.com>
2025-12-20 01:44:21 +0200

    Create Magic (math).cpp

---
+++ Magic (math).cpp
@@ -0,0 +1,55 @@
+/* ----------- Miller-Rabin ----------- */
+ 
+bool isPrime(ll n) {
+    if (n < 2) return false;
+    for (ll p : {2, 3, 5, 7, 11, 13, 17, 19, 23}) {
+        if (n == p) return true;
+        if (n % p == 0) return n == p;
+    }
+ 
+    ll d = n - 1, s = 0;
+    while ((d & 1) == 0) d >>= 1, s++;
+ 
+    for (ll a : {2, 325, 9375, 28178, 450775, 9780504, 1795265022}) {
+        if (a % n == 0) continue;
+        ll x = pow_mod(a, d, n);
+        if (x == 1 || x == n - 1) continue;
+        bool comp = true;
+        for (int r = 1; r < s; r++) {
+            x = mul_mod(x, x, n);
+            if (x == n - 1) {
+                comp = false;
+                break;
+            }
+        }
+        if (comp) return false;
+    }
+    return true;
+}
+
+------------------------------------------------------------------------------------------
+
+/* ----------- Pollard Rho ----------- */
+ 
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+ll Pollard(ll n) {
+    if (n % 2 == 0) return 2;
+    if (n % 3 == 0) return 3;
+ 
+    ll c = uniform_int_distribution<ll>(1, n - 1)(rng);
+    ll x = uniform_int_distribution<ll>(0, n - 1)(rng);
+    ll y = x, d = 1;
+ 
+    auto f = [&](ll x) {
+        return (mul_mod(x, x, n) + c) % n;
+    };
+ 
+    while (d == 1) {
+        x = f(x);
+        y = f(f(y));
+        d = gcd(abs(x - y), n);
+    }
+ 
+    return d == n ? Pollard(n) : d;
+}
