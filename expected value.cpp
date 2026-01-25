4907a0c42492f037dd2fe1ac7a00980e7b1962e6
AhmedPlusPlus <157212041+AhmedMostafa81@users.noreply.github.com>
2026-01-25 15:20:51 +0200

    Create expected value.cpp

---
+++ expected value.cpp
@@ -0,0 +1,12 @@
+/*
+linearity of expectations :
+exp(a + b) = exp(a) + exp(b)   -> a,b are independent or not
+exp(a * b) = exp(a) * exp(b)   -> a,b are independent
+
+Independence means that 
+Does knowing 'a' change the distribution of 'b'?
+
+
+exp (a / b) != exp(a) / exp(b)
+
+*/
