5782c8bce9cf3b7a100788ae05d4c2104f74a9d3
AhmedPlusPlus <157212041+AhmedMostafa81@users.noreply.github.com>
2026-02-15 06:23:22 +0200

    Update matrix exponentiation.cpp

--- matrix exponentiation.cpp
+++ matrix exponentiation.cpp
@@ -72,3 +72,73 @@ int main()
     return 0;
 }
 
+
+
+
+
+
+
+//     floyd
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+typedef vector<vector<int>> matrix;
+#define init(name,n,m,x) matrix name (n,vector<int>(m,x))
+#define AhmedPlusPlus ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define hi cerr<<"HI\n";
+
+/*                        ->    NO CLEAN CODE HERE    <-                        */
+
+const int inf = 3e18;
+
+matrix mul(matrix a , matrix &b)
+{
+    int n = a.size() , m = b[0].size();
+    init(res, n,m,inf);
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            for(int k=0; k<n; k++)
+            {
+                res[i][j] = min(res[i][j],a[i][k]+b[k][j]);
+            }
+        }
+    }
+    return res;
+}
+
+matrix matrix_fp(matrix &a , int p)
+{
+    if (p==1)
+        return a;
+
+    matrix res = matrix_fp(a , p/2);
+    res = mul(res , res);
+    if (p%2) res = mul(res , a);
+
+    return res;
+}
+
+signed main() {
+/* ^^^ */    AhmedPlusPlus    /* ^^^ */
+
+//    ->> practice makes perfect
+
+    int n,m,k;cin>>n>>m>>k;
+    init(base_case,n,1,0);
+    init(T,n,n,inf);
+    for(int i = 0,x,y,w;i <m;i++){
+        cin>>x>>y>>w;
+        x--,y--;
+        T[y][x] = min(T[y][x],w);
+    }
+    matrix res = mul(matrix_fp(T,k),base_case);
+    int ans = inf;
+    for (int i =0;i<n;i++)ans = min(ans , res[i][0]);
+    if (ans > 2e18+5)cout << "IMPOSSIBLE" ;
+    else cout << ans << '\n';
+
+}
