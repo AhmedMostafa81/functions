02fd240f5295cdea40d60d92bf2083f034bbd5f7
AhmedPlusPlus <157212041+AhmedMostafa81@users.noreply.github.com>
2026-03-05 05:53:31 +0200

    Update Persistent Segment Tree.cpp

--- Persistent Segment Tree.cpp
+++ Persistent Segment Tree.cpp
@@ -1,68 +1,80 @@
-const int N = 2e5+5;
-int a[N];
 
-struct V {
-    V *l, *r;
-    long long sum;
+const int N = 3e4+10;
+int fr[N];
 
-    V(int val) : l(nullptr), r(nullptr), sum(val) {}
-    V(V *l, V *r) : l(l), r(r), sum(0) {
-        if (l) sum += l->sum;
-        if (r) sum += r->sum;
+struct Node {
+    Node *l , *r;
+    int sum = 0;
+    Node(int val):l(nullptr) , r(nullptr) , sum(val){}
+    Node(Node *l ,Node *r) :l(l) , r(r) , sum(0) {
+        if (l) sum += l -> sum;
+        if (r) sum += r -> sum;
     }
 };
 
-V* build( int tl, int tr) {
-    if (tl == tr)
-        return new V(a[tl]);
-    int tm = (tl + tr) / 2;
-    return new V(build( tl, tm), build(tm+1, tr));
-}
+struct persistent_segment_tree {
+    Node* build(int st ,int ed) {
+        if (st == ed)
+            return new Node(fr[st]);
+        int mid = (st + ed) >> 1;
+        return new Node(build(st , mid) , build(mid + 1 , ed));
+    }
+    Node* update(Node* v, int st , int ed , int pos , int new_fr) {
+        if (st == ed)
+            return new Node(new_fr);
+        int mid = (st + ed) >> 1;
+        if (pos <= mid)
+            return new Node(update(v->l , st , mid , pos , new_fr) ,v->r );
+        else
+            return new Node(v->l , update(v->r , mid + 1 , ed , pos , new_fr)  );
+    }
+    int query(Node* L , Node* R , int st , int ed , int k) {
+        if (st >= k)
+            return R->sum - L->sum;
+        if (ed < k)
+            return 0;
+        int mid = (st + ed) >> 1;
+        return query(L->l , R->l , st , mid , k) + query(L -> r , R -> r , mid + 1 , ed , k );
+    }
+};
 
-long long get_sum(V* v, int tl, int tr, int l, int r) {
-    if (l > r)
-        return 0;
-    if (l == tl && tr == r)
-        return v->sum;
-    int tm = (tl + tr) / 2;
-    return get_sum(v->l, tl, tm, l, min(r, tm))
-         + get_sum(v->r, tm+1, tr, max(l, tm+1), r);
-}
-
-V* update(V* v, int tl, int tr, int pos, int new_val) {
-    if (tl == tr)
-        return new V(new_val);
-    int tm = (tl + tr) / 2;
-    if (pos <= tm)
-        return new V(update(v->l, tl, tm, pos, new_val), v->r);
-    else
-        return new V(v->l, update(v->r, tm+1, tr, pos, new_val));
-}
+int a[N] ;
 
 void maybe() {
 
-    int n , q;cin >> n >> q;
-    for (int i  = 0; i < n ; i++)
-        cin >> a[i];
-    vector<V*>roots;
-    roots.push_back(build(0,n-1));
+    int n ; cin >> n ;
+    set<int>st;
+    for (int i = 0; i < n ; i++)
+        cin >> a[i] , st.insert(a[i]);
+    int cur = 0 ;
+    map<int,int>id;
+    for (auto num:st)
+        id[num] = cur++;
+    persistent_segment_tree seg;
+    vector<Node*>v = {seg.build(0,n+5)};
+    for (int  i = 0 ; i < n ; i++) {
+        fr[id[a[i]]]++;
+        v.push_back(seg.update(v.back() , 0 , n+5 , id[a[i]] , fr[id[a[i]]]));
+    }
+    int q;cin >> q;
+    int last_ans = 0 ;
     while (q--) {
-        int op , k , a, b , x; cin >> op;
-        if (op == 1) {
-            cin >> k >> a >> x;
-            --k , --a;
-            roots[k] = update(roots[k] , 0,n-1,a , x);
-        }
-        else if (op == 2) {
-            cin >> k >> a >> b;
-            --k , --a , --b;
-            cout << get_sum(roots[k] ,0,n-1, a , b) << '\n';
-        }
+        int a , b , c; cin >> a >> b >> c;
+        int i = a ^ last_ans;
+        int j = b ^ last_ans;
+        int k = c ^ last_ans;
+        i = max(i , 1);
+        j = min(j , n);
+        if (i > j)
+            last_ans = 0 ;
         else {
-            cin >> k ;
-            --k;
-            roots.push_back(roots[k]);
+            auto it = st.upper_bound(k);
+            if (it != st.end())
+                last_ans = seg.query(v[i-1],v[j],0,n+5,id[*it]);
+            else
+                last_ans = 0;
         }
+        cout << last_ans << '\n';
     }
 
 }
