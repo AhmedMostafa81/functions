6d1d2fd2126ca4904990663b0935645d7c9d4d6e
AhmedPlusPlus <157212041+AhmedMostafa81@users.noreply.github.com>
2026-04-16 18:51:12 +0200

    Update General.cpp

--- General.cpp
+++ General.cpp
@@ -1,2 +1,11 @@
+ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+----------------------------------------------------------
 //freopen("input.txt", "r", stdin);
 //freopen("output.txt", "w", stdout);
+----------------------------------------------------------
+  //   make vector unique
+    
+vector<int> rem;
+sort(rem.begin(), rem.end());
+rem.erase(unique(rem.begin(), rem.end()), rem.end());   
+----------------------------------------------------------
