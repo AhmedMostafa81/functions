06ff7dc1bd6c087ce7ad2565bb3d2afeb1f35d38
AhmedPlusPlus <157212041+AhmedMostafa81@users.noreply.github.com>
2026-04-16 21:09:21 +0200

    Create Bellman-Ford.cpp

---
+++ Graph/Bellman-Ford.cpp
@@ -0,0 +1,37 @@
+void solve()
+{
+    vector<int> d(n, INF);
+    d[v] = 0;
+    vector<int> p(n, -1);
+    int x;
+    for (int i = 0; i < n; ++i) {
+        x = -1;
+        for (Edge e : edges)
+            if (d[e.a] < INF)
+                if (d[e.b] > d[e.a] + e.cost) {
+                    d[e.b] = max(-INF, d[e.a] + e.cost);
+                    p[e.b] = e.a;
+                    x = e.b;
+                }
+    }
+
+    if (x == -1)
+        cout << "No negative cycle from " << v;
+    else {
+        int y = x;
+        for (int i = 0; i < n; ++i)
+            y = p[y];
+
+        vector<int> path;
+        for (int cur = y;; cur = p[cur]) {
+            path.push_back(cur);
+            if (cur == y && path.size() > 1)
+                break;
+        }
+        reverse(path.begin(), path.end());
+
+        cout << "Negative cycle: ";
+        for (int u : path)
+            cout << u << ' ';
+    }
+}
